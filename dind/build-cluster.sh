8de237bd57a31df67dbdc07f4875eaffa75209d9
James Munnelly <james.munnelly@jetstack.io>
2018-05-09 23:11:11 +0100

    Update dind to support consuming upstream kubernetes releases

--- dind/build-cluster.sh
+++ dind/build-cluster.sh
@@ -19,48 +19,23 @@ set -o nounset
 set -o pipefail
 
 TOOL_ROOT=${TOOL_ROOT:-"$(pwd)"}
-KUBE_ROOT=${KUBE_ROOT:-"../../kubernetes"}
 
 # Create a tmpdir for the Docker build's context.
 CLUSTER_DIR=$(mktemp -d)
 echo "Building cluster docker image from ${CLUSTER_DIR}"
 
-
-cd ${KUBE_ROOT}
-
-# The outer layer doesn't run node or master components. But tests need kubectl.
-bazel build //build/debs:kubectl
-
-# Tests are run from the outer layer, so package the test artifacts.
-bazel build //vendor/github.com/onsi/ginkgo/ginkgo:ginkgo
-bazel build //test/e2e:e2e.test
-
-cd -
-
-# Copy artifacts into the tmpdir for Docker's context.
-
-cp ${KUBE_ROOT}/bazel-bin/build/debs/kubectl.deb ${CLUSTER_DIR}
-
-# Tests are only run against one platform (linux/amd64), so no searching logic.
-cp ${KUBE_ROOT}/bazel-bin/vendor/github.com/onsi/ginkgo/ginkgo/linux_amd64_stripped/ginkgo ${CLUSTER_DIR}
-cp ${KUBE_ROOT}/bazel-bin/test/e2e/e2e.test ${CLUSTER_DIR}
-
-# Get version info in a file. Kubeadm version and docker tags might vary slightly.
-cat ${KUBE_ROOT}/bazel-out/stable-status.txt | grep STABLE_BUILD_SCM_REVISION | awk '{print $2}' > ${CLUSTER_DIR}/source_version
-cat ${KUBE_ROOT}/bazel-out/stable-status.txt | grep STABLE_DOCKER_TAG | awk '{print $2}' > ${CLUSTER_DIR}/docker_version
+K8S_VERSION=$1
 
 # Get systemd wrapper (needed until we put cluster-up into a systemd target).
 cp ${TOOL_ROOT}/init-wrapper.sh ${CLUSTER_DIR}
 # Get the cluster-up script.
 cp ${TOOL_ROOT}/start.sh ${CLUSTER_DIR}
-# Get the test execution script.
-cp ${TOOL_ROOT}/dind-test.sh ${CLUSTER_DIR}
 
 cp ${TOOL_ROOT}/cluster/Dockerfile ${CLUSTER_DIR}/Dockerfile
 cp ${TOOL_ROOT}/cluster/Makefile ${CLUSTER_DIR}/Makefile
 
 # Create the dind-cluster container
 cd ${CLUSTER_DIR}
-docker save gcr.io/google-containers/dind-node-amd64:$(cat docker_version) -o dind-node-bundle.tar
-make build K8S_VERSION=$(cat docker_version)
+docker save eu.gcr.io/jetstack-build-infra/dind-node-amd64:${K8S_VERSION} -o dind-node-bundle.tar
+make build K8S_VERSION=${K8S_VERSION}
 cd -
